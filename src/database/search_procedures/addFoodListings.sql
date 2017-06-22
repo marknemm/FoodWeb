510f64b8018b11a13a96151b75843e3344cc0d64
Mark Nemmer <marknemm@buffalo.edu>
2017-06-21 20:57:09 -0400

    CHange AddFoodListings to AddFoodListing

--- src/database/search_procedures/addFoodListings.sql
+++ src/database/search_procedures/addFoodListings.sql
@@ -1,23 +1,20 @@
 /**
- * A basic function for adding one or more food listings.
+ * A basic function for adding a food listing.
  */
-CREATE OR REPLACE FUNCTION AddFoodListings
+CREATE OR REPLACE FUNCTION AddFoodListing
 (
-    _foodTypeKey        INTEGER[]         DEFAULT NULL,   -- What Food Type is this?
-    _perishable         BOOLEAN[]         DEFAULT NULL,   -- Is the food perishable?
-    _expireDate         TEXT[]            DEFAULT NULL    -- The expiration date of the food.
+    _foodTypeKey        INTEGER         DEFAULT NULL,   -- What Food Type is this?
+    _perishable         BOOLEAN         DEFAULT NULL,   -- Is the food perishable?
+    _expireDate         TEXT            DEFAULT NULL    -- The expiration date of the food.
 )
 RETURNS VOID
 AS $$
-    DECLARE i INTEGER;
 BEGIN
     
-    FOR i IN ARRAY_LOWER(_foodTypeKey, 1) .. ARRAY_UPPER(_foodTypeKey, 1)
-    LOOP
-        raise notice 'Values: %, %, %', _foodTypeKey[i], _perishable[i], _expireDate[i];
-    END LOOP;
+    raise notice 'Values: %, %, %', _foodTypeKey, _perishable, _expireDate;
+    -- TODO: insert each entry into the FoodListing table.
 
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT AddFoodListings('{1, 2}', '{true, false}', '{1/2/2021, 1/2/2022}');
+SELECT AddFoodListing(1, true, '1/2/2021');
