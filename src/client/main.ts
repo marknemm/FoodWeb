8846cc4b468564e4b643e917a9fab67b015ea0ff
Mark Nemmer <marknemm@buffalo.edu>
2017-06-22 21:34:56 -0400

    Reorganized code

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './client/app.module';
+import { AppModule } from './app.module';
 
 if (environment.production) {
   enableProdMode();
--- src/server/index.js
+++ src/server/index.js
@@ -4,14 +4,18 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../../dist'));
+
+const path = require('path');
+
+//app.use(express.static(__dirname + '/../../public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/../../views/');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(path.join(__dirname + '/../../dist/index.html'));
 });
 
 app.get('/times', function(request, response) {
