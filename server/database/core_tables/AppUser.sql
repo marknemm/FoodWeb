2d19ee7865c6cca2c24fb7bc24f049a353d35e17
Mark Nemmer <marknemm@buffalo.edu>
2017-07-04 11:32:48 -0400

    Changed Donor and Receiver table definitions to DonorOrganization and ReceiverOrganization. Also, updated definition of login function. FInally, added fields to AppUser table and changed some existing field names there.

--- server/database/core_tables/AppUser.sql
+++ server/database/core_tables/AppUser.sql
@@ -4,27 +4,48 @@ CREATE TABLE IF NOT EXISTS AppUser
     appUserKey SERIAL PRIMARY KEY
 );
 
--- Email will be the username also.
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS appUserEmail           VARCHAR(128)    UNIQUE;
+-- Unique Email address.
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS email                      VARCHAR(128)    NOT NULL UNIQUE;
 
 -- Password must be encrypted when entered into this table!
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS appUserPassword        TEXT            NOT NULL;
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS password                   TEXT            NOT NULL;
 
---A unique userName for every user. This, along with the email shall be the primary identifiers
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS userName               VARCHAR(128)    NOT NULL UNIQUE; 
+--A unique username for every user. This, along with the email shall be the primary identifiers
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS username                   VARCHAR(128)    NOT NULL UNIQUE; 
 
 -- The App User's Last Name.
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS appUserLastName        VARCHAR(60)     NOT NULL;
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS lastName                   VARCHAR(60)     NOT NULL;
 
 -- The App User's First Name.
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS appUserFirstName       VARCHAR(60)     NOT NULL;
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS firstName                  VARCHAR(60)     NOT NULL;
+
+-- The App User's phone number.
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS phone                      CHAR(12);
+
+-- The App User's Street Address.
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS address                    VARCHAR(128);
+
+-- City name.
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS city                       VARCHAR(60);
+
+-- Zip code.
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS zip                        INTEGER;
+
+-- Two letter state abbreviation.
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS state                      CHAR(2);
 
 -- Foreign key to the Donor table.
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS donorKey               INTEGER         REFERENCES Donor (donorKey);
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS donorOrganizationKey       INTEGER         REFERENCES DonorOrganization (donorOrganizationKey);
 
 -- Foreign key to the Receiver table.
-ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS receiverKey            INTEGER         REFERENCES Receiver (receiverKey);
+ALTER TABLE AppUser ADD COLUMN IF NOT EXISTS receiverOrganizationKey    INTEGER         REFERENCES ReceiverOrganization (receiverOrganizationKey);
 
 
--- Index for quick sorting and searching.
-CREATE INDEX IF NOT EXISTS appUserFullNameIdx ON AppUser (appUserLastName, appUserFirstName);
+-- Index on username.
+CREATE UNIQUE INDEX IF NOT EXISTS appUserUsernameIdx ON AppUser (username);
+
+-- Index on email.
+CREATE UNIQUE INDEX IF NOT EXISTS appUserEmailIdx ON AppUser (email);
+
+-- Index for quick sorting and searching by full name.
+CREATE INDEX IF NOT EXISTS appUserFullNameIdx ON AppUser (lastName, firstName);
--- server/database/stored_procedures/LogIn.sql
+++ server/database/stored_procedures/LogIn.sql
@@ -1,30 +1,30 @@
 /*This SP should, given a userName or email, search the AppUser TABLE 
     for the given user. If user is not found, it will return NULL. If user is found, 
     it will then check the password. If passwrod does not match, it will return NULL, 
-    else, it will return the appUserKey for the given user*/ 
+    else, it will return the appUserKey, username, and email for the given user*/ 
 
 SELECT dropFunction('login');
 CREATE OR REPLACE FUNCTION login
 (
-    _identifier         VARCHAR(128)        DEFAULT NULL,
-    _password           TEXT                DEFAULT NULL
+    _identifier         VARCHAR(128)    DEFAULT NULL, -- This will either be username or email.
+    _password           TEXT            DEFAULT NULL  -- This should be the salted hash of the password supplied by the user.
 )
-RETURNS INTEGER
+RETURNS TABLE
+(
+    appUserKey     INTEGER,
+    username       VARCHAR(128),
+    email          VARCHAR(128)
+)
 AS $$
-    DECLARE returnKey INTEGER;
-    DECLARE storedPassword TEXT;
 BEGIN
-
-    returnKey := (SELECT appUserKey FROM appUser WHERE _identifier = userName OR _identifier = appUserEmail);
-    IF returnKey IS NOT NULL THEN
-        storedPassword := (SELECT appUserPassword FROM appUser WHERE returnKey = appUserKey);
-        IF _password = storedPassword THEN
-            RETURN returnKey;
-        END IF;
-    END IF;
-
-    -- We could not match username and password.
-    RETURN NULL;
+    RETURN QUERY
+    SELECT  AppUser.appUserKey,
+            AppUser.username,
+            AppUser.email
+    FROM    AppUser
+    WHERE   (   AppUser.username = _identifier
+             OR AppUser.email = _identifier)
+      AND   AppUser.password = _password;
 END;
 $$ LANGUAGE plpgsql;
 
