02cabf340c6ccdcf145ab826cd9f2b6aa1ac55ba
Akash Ghose <aghose@buffalo.edu>
2017-07-06 20:08:57 -0400

    stalk

--- server/src/authentication/authentication_model.ts
+++ server/src/authentication/authentication_model.ts
@@ -1,17 +1,6 @@
 'use strict';
 var connectionPool = require('../database_help/connection_pool');
 var passHashUtil = require('./password_hash_util');
-var appUser = require('AppUser.sql')
-var insertIntoAppUser = require('InsertIntoAppUser.sql')
-var express = require('express');
-var app = express();
-
-var con = connectionPool.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
-});
 
 /**
  * Model for authentication business logic. Contains functionality for authenticating or logging a user in, logging out, and signing up.
@@ -26,32 +15,31 @@ export class AuthenticationModel {
 
     }
 
-    public SignUpUser(email,password,salt,username,lastname,firstname,phone,address,city,zip,state,dOrg,rOrg){
-        if (this.isValidEmail(email)){
-            if(this.isValidPassword(password)){
-                app.get('/db', function(request, response) {
-                // Grab a connection. This comes in the form of a Promise.
-                    connectionPool.connect().then(client => {
-                        // Grabbing a connection succeeded. Now we can execute a query using a callback function.
-                        //client.query('SELECT * FROM test_table;', function(err, result) {
-                        insertIntoAppUser.insertIntoAppUser(// S.O.S. How to pass args?)
-                            //if (err) {
-                            //    console.error(err);
-                            //    response.send('Error ' + err);
-                            //}
-                            //else {
-                            //   response.send({results: result.rows});
-                            //}
-                        );
-                    } // end connectionPool.connect 
-                //} // end function
-            } // end ifValidPassword
-        } // end ifValidEmail
+    public SignUpUser(email,password,salt,username,lastname,firstname){
+        if (this.isValidEmail(email) && this.isValidPassword(password)){
+            // Grab a connection. This comes in the form of a Promise.
+            connectionPool.connect().then(client => {
+                // Grabbing a connection succeeded. Now we can execute a query using a callback function.
+                // The client is the result of the promise. It is just something we can run sql on.
+                var queryString = 'SELECT insertIntoAppUser ($1, $2, $3, $4, $5, $6);';
+                var queryArgs = [email, password, salt, username, lastname, firstname];
+                client.query(queryString, queryArgs).then(result => {
+                    console.log('Yay! It worked' + result);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+                        
     } // end signUpUser
 
 
     //currently just checks if email contains an @
-    public isValidEmail(email){
+    private isValidEmail(email){
         var length = email.length;
         for(var i = 0; i < length; i++) {
         if(email[i] == '@')
