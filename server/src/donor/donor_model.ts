a3ca2d773ee08004f72f1af3a3e724a5a1ea5572
Aamel Unia <aameluni@buffalo.edu>
2017-07-08 14:22:51 -0400

    Skeleton of donor controller and donor model updated

--- server/src/donor/donor_model.ts
+++ server/src/donor/donor_model.ts
@@ -1,4 +1,7 @@
 'use strict';
+var multer = require('multer');
+var upload = multer({dest: __dirname+'../../donoruploads'})
+
 
 /**
 Donor model which contains the business logic for handling the donor's submissions.
@@ -10,8 +13,13 @@ export class DonorModel {
     }
    
    //interprets the JSON data recieved from the frontend and adds information recieved to the FoodListing table.
-    public intepretData(){
-        //TODO
+    public intepretData(donorsubmission){
+        var foodTypeKey = donorsubmission.foodTypeKey;
+        var perishable = donorsubmission.perishable;
+        var postedByAppUserKey = donorsubmission.postedByAppUserKey;
+        var foodDescription = donorsubmission.foodDescription;
+        var imgstring = donorsubmission.img;
+        var expireDate = donorsubmission.expireDate;
    }
 
 };
