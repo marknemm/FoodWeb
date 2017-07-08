ce2833b8995feaf336420d07d8dc6c31bbffbdab
Mark Nemmer <marknemm@buffalo.edu>
2017-07-08 09:42:57 -0400

    Moved Donor Component, teamplate, and shared data into client/src/

--- client/src/app/donor/donor.component.ts
+++ client/src/app/donor/donor.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ValidationService } from '../shared/validation.service';
+import { Food } from '../shared/food';
 
 @Component({
-  selector: 'app-donor',
-  templateUrl: './donor.component.html',
-  styleUrls: ['./donor.component.css']
+  moduleId: module.id,
+  selector: 'donor',
+  templateUrl: 'donor.component.html'
 })
 export class DonorComponent implements OnInit {
+  foodForm: FormGroup;
+  model: Food;
+  submittedModel: Food;
+  pornp: string[];
+  rornr: string[];
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+      this.model = new Food(15, 'Spaghetti', '1', 'Bassett Park', 'Perishable', 'Not Refrigerated');
+
+      this.pornp = ['Perishable', 'Not Perishable'];
+      this.rornr = ['Refrigerated', 'Not Refrigerated'];
+
+      this.foodForm = this.formBuilder.group({
+        name:     [this.model.name, Validators.required],
+        quantity: [this.model.quantity, Validators.required],
+        location:    [this.model.location, Validators.required],
+        pornp:    [this.model.pornp, Validators.required],
+        rornr: [this.model.rornr, Validators.required]
+      });
   }
 
+  onSubmit({ value, valid }: { value: Food, valid: boolean }) {
+    this.submitted = true;
+    this.submittedModel = value;
+  }
 }
