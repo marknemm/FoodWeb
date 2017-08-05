af8aa2ce3b5ca4028ee463c10e031f1e616de5bd
Akash Ghose <aghose@buffalo.edu>
2017-08-05 17:02:39 -0400

    Get Food listing should be working as before. New additions are not functional yet.

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ var connectionPool = require('./database_help/connection_pool');
 // Our controllers that will handle requests after this router hands off the data to them.
 import { AuthenticationController } from './authentication/authentication_controller';
 import { AuthenticationModel} from './authentication/authentication_model';
-import { handleAddFoodListingRequest, handleGetFoodListingsRequest, handleClaimFoodListingRequest } from './food-listing/food-listing-controller';
+import { handleAddFoodListingRequest, handleReceiverGetFoodListingsRequest, handleClaimFoodListingRequest } from './food-listing/food-listing-controller';
 
 // This is where compiled client ts files will go. We need this to locate index.html!
 const clientBuildDir = __dirname + '/../../client/dist/';
@@ -53,7 +53,7 @@ app.get('/authentication/logout', authenticationController.logout.bind(authentic
 app.post('/donor/addFoodListing', handleAddFoodListingRequest);
 
 // Handle /receiver/getFoodListings route by passing off to FoodListingController.
-app.post('/receiver/getFoodListings', handleGetFoodListingsRequest);
+app.post('/receiver/getFoodListings', handleReceiverGetFoodListingsRequest);
 
 // Handle /receiver/claimFoodListing route by passing off to FoodListingController.
 app.post('/receiver/claimFoodListing', handleClaimFoodListingRequest);
