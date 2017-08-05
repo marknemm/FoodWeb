f1d145d2e3c9c3a6d192bdd7da3c43dca07e2664
Akash Ghose <aghose@buffalo.edu>
2017-08-05 13:59:20 -0400

    Fixed Compiler issuses for foodlisting (hopefully). Should complie, but still incomplete. Get Food lisiting will not work.

--- server/src/food-listing/get-food-listings.ts
+++ server/src/food-listing/get-food-listings.ts
@@ -8,15 +8,15 @@ export function getFoodListing(foodObject): Promise<Array<object>> {
     var foodTypesArg: string = generateFoodTypesArg(foodObject);
     var expireDateArg: string = generateExpireDateArg(foodObject);
     var queryArgs: Array<any> = new Array<any>();
-    let requesetedByAppUserKey: number = generateRequestedByAppUserKey(foodObject, sessionObject);
+   // let requesetedByAppUserKey: number = generateRequestedByAppUserKey(foodObject, sessionObject);
 
-    if(requesetedByAppUserKey == -1){
-        return Promise.reject(new Error("You need to Log in"));
-    }
+    // if(requesetedByAppUserKey == -1){
+    //     return Promise.reject(new Error("You need to Log in"));
+    // }
 
     // Build our prepared statement.
     var queryString: string = 'SELECT * FROM getFoodListings(null, $1, $2, null, $3, $4);';
-    queryArgs = [ foodTypesArg, perishableArg, expireDateArg, requesetedByAppUserKey ];
+    queryArgs = [ foodTypesArg, perishableArg, expireDateArg, null ];
     queryString = fixNullQueryArgs(queryString, queryArgs);
 
     // Log and execute query.
