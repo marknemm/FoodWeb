a42383fcf06d1a36e09841832e5a32f9f425ca1d
Aamel Unia <aameluni@buffalo.edu>
2017-08-21 03:01:48 -0400

    Added User Verification Functionality: Now can send verification emails

--- server/src/authentication/app-user-signup.ts
+++ server/src/authentication/app-user-signup.ts
@@ -9,6 +9,9 @@ import { fixNullQueryArgs } from "../database-help/prepared-statement-helper";
 import { Validation } from '../../../shared/common-util/validation';
 import { AppUserInfo } from '../../../shared/authentication/app-user-info';
 
+var randomstring = require('randomstring');
+
+var nodemailer = require('nodemailer');
 
 /**
  * Performs the signup for a new app user.
@@ -41,6 +44,11 @@ export function signup(appUserSignupInfo: AppUserInfo): Promise<AppUserInfo> {
         })
         .then((insertQueryResult: QueryResult) => {
             return handleSignUpUserResult(appUserSignupInfo, insertQueryResult);
+        })
+        .then((appUserSignupInfo: AppUserInfo)=> {
+            let stringToken: String = stringTokenGenerator();
+            let userType: String = insertIntoUnverifiedAppUser(appUserSignupInfo, stringToken)
+            return sendUserEmail(appUserSignupInfo,stringToken, userType)
         })
         .catch((err: Error) => {
             console.log(err);
@@ -78,7 +86,7 @@ function insertIntoAppUser(appUserSignupInfo: AppUserInfo, hashedPassword: strin
 }
 
 /**
- * Analyzes and hndles the result of the insert into AppUser query. Generates the final result of the signup operation.
+ * Analyzes and handles the result of the insert into AppUser query. Generates the final result of the signup operation.
  * @param appUserSignupInfo The .
  * @param insertQueryResult The result of the insert of the new user into the AppUser table.
  */
@@ -93,4 +101,88 @@ function handleSignUpUserResult(appUserSignupInfo: AppUserInfo, addAppUserResult
     else {
         return Promise.reject(new Error('Signup failed. Incorrect result returned form addAppUser SQL function.'));
     }
+}
+
+
+function insertIntoUnverifiedAppUser(appUserSignupInfo: AppUserInfo, stringToken: String) : String {
+
+        let queryString: string = 'SELECT * FROM addUnverifiedAppUser($1, $2)';
+        let queryArgs: Array<any> = [ appUserSignupInfo.appUserKey,
+                                      stringToken ];
+        
+        query(queryString, queryArgs);
+
+        if (appUserSignupInfo.organizationKey) {
+            return 'Organization'
+        }
+        else {
+            return 'Individual'
+        }
+
+}
+
+
+function sendUserEmail(appUserSignupInfo: AppUserInfo, stringToken: String, userType: String) : Promise<AppUserInfo>{
+
+    let verificationLink = 'http://connect-food.herokuapp.com/authentication/verify?token='+stringToken
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: false,
+        port: 25,
+        auth : {
+            user: 'foodweb.noreply@gmail.com',
+            pass: 'connect-food!1'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    
+    if (userType === 'Individual'){
+
+        let mailOptions = {
+            from: '"Food Web" <foodweb.noreply@gmail.com',
+            to: appUserSignupInfo.email,
+            subject: 'Verify Your Account With Food Web',
+            html: 'Dear User,<br><br>Welcome to Food Web!<br><br>Please click <a href ="'+verificationLink+'">here</a> to verify your account with us.<br><br>Thank you,<br><br>The Food Web Team'
+        }
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                return Promise.reject(new Error('SignUp failed. Unable to send verification email.'));
+            } else {
+                console.log('Email sent!');
+            }
+        });
+
+    }
+
+    else {
+
+        let mailOptions = {
+            from: '"Food Web" <foodweb.noreply@gmail.com',
+            to: 'foodweb.noreply@gmail.com',
+            subject: 'A New Organization Has Signed Up With Food Web: Please Verify',
+            html: 'Dear User,<br><br>A new organization: '+appUserSignupInfo.organizationName+' has signed up With Food Web!<br><br>Their phone number is '+appUserSignupInfo.phone+' and their email is '+appUserSignupInfo.email+'.<br><br>Please click <a href ="'+verificationLink+'">here</a> to officially verify their account once their identity is validated.<br><br>Thank you,<br><br>The Food Web Team'
+        }
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                return Promise.reject(new Error('SignUp failed. Unable to send verification email.'));
+            } else {
+                console.log('Email sent!');
+            }
+        });
+
+
+    }
+   
+    return Promise.resolve(appUserSignupInfo);
+
+}
+
+
+function stringTokenGenerator(): String{
+    return randomstring.generate(20);
 }
