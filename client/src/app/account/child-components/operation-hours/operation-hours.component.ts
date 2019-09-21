e26d21893b0fca829d7249faa1263cd0e9bdac27
Mark <marknemmer@gmail.com>
2019-09-21 14:24:12 -0400

    Add animation to width/height of operation hours

--- client/src/app/account/child-components/operation-hours/operation-hours.component.ts
+++ client/src/app/account/child-components/operation-hours/operation-hours.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input, Optional } from '@angular/core';
-import { FormGroupDirective, FormControl } from '@angular/forms';
+import { Component, OnInit, OnChanges, Input, Optional, OnDestroy, SimpleChanges } from '@angular/core';
+import { FormGroupDirective } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { TypedFormControl } from '../../../data-structure/typed-form-control';
 import { OperationHoursInfoForm } from '../../forms/operation-hours-info.form';
 import { OperationHoursArray } from '../../forms/operation-hours.array';
 import { OperationHoursForm } from '../../forms/operation-hours.form';
@@ -8,19 +11,23 @@ import { ConfirmDialogService } from '../../../shared/services/confirm-dialog/co
 import { DateTimeService } from '../../../date-time/services/date-time/date-time.service';
 import { FormHelperService } from '../../../shared/services/form-helper/form-helper.service';
 import { OperationHours } from '../../../../../../shared/src/interfaces/account/operation-hours';
-import { TypedFormControl } from 'src/app/data-structure/typed-form-control';
 
 @Component({
   selector: 'food-web-operation-hours',
   templateUrl: './operation-hours.component.html',
   styleUrls: ['./operation-hours.component.scss']
 })
-export class OperationHoursComponent implements OnInit {
+export class OperationHoursComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() editing = false;
   @Input() operationHoursArr: OperationHours[] = [];
   @Input() formGroupName: string;
   @Input() formGroup: OperationHoursInfoForm;
+
+  private _operationHoursGridHeight: number;
+  private _operationHoursGridOpacity: number;
+  private _formFieldTabIndex: number;
+  private _destroy$ = new Subject();
 
   constructor(
     public constantsService: ConstantsService,
@@ -39,6 +46,55 @@ export class OperationHoursComponent implements OnInit {
     return limitedOpHoursCtrl ? limitedOpHoursCtrl.value : false;
   }
 
+  get operationHoursGridHeight(): number {
+    return this._operationHoursGridHeight;
+  }
+
+  get operationHoursGridOpacity(): number {
+    return this._operationHoursGridOpacity;
+  }
+
+  get formFieldTabIndex(): number {
+    return this._formFieldTabIndex;
+  }
+
+  ngOnInit() {
+    this.formGroup = <OperationHoursInfoForm>this._formHelperService.deriveFormGroup(this.formGroup, this.formGroupName, this._formGroupDirective);
+    if (this.operationHoursArr && this.operationHoursArr.length > 0) {
+      this.formGroup.patchValue(this.operationHoursArr);
+    }
+    this._recalcOperationHoursGridStyles = this._recalcOperationHoursGridStyles.bind(this);
+    this.formGroup.valueChanges.pipe(
+      takeUntil(this._destroy$)
+    ).subscribe(this._recalcOperationHoursGridStyles);
+    window.addEventListener('resize', this._recalcOperationHoursGridStyles);
+    this._recalcOperationHoursGridStyles();
+  }
+
+  private _recalcOperationHoursGridStyles(): void {
+    const numEntries: number = this.formGroup.get('operationHours').value.length;
+    this._operationHoursGridHeight = (this.editing && this.operationHoursLimited)
+      ? (window.innerWidth > 991)
+        ? (Math.ceil(numEntries / 2) * 84)
+        : (window.innerWidth > 410)
+          ? (numEntries * 84)
+          : (numEntries * 130)
+      : 0;
+    this._operationHoursGridOpacity = (this._operationHoursGridHeight > 0) ? 1 : 0;
+    this._formFieldTabIndex = (this._operationHoursGridHeight > 0) ? 0 : -1;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.editing) {
+      setTimeout(() => this._recalcOperationHoursGridStyles());
+    }
+  }
+
+  ngOnDestroy() {
+    this._destroy$.next();
+    window.removeEventListener('resize', this._recalcOperationHoursGridStyles);
+  }
+
   getDefaultStartTime(operationHoursForm: OperationHoursForm): string {
     const curStartTimeVal: string = operationHoursForm.get('startTime').value;
     return (curStartTimeVal)
@@ -51,12 +107,5 @@ export class OperationHoursComponent implements OnInit {
     return (curEndTimeVal)
       ? curEndTimeVal
       : '5:00 PM';
-  }
-
-  ngOnInit() {
-    this.formGroup = <OperationHoursInfoForm>this._formHelperService.deriveFormGroup(this.formGroup, this.formGroupName, this._formGroupDirective);
-    if (this.operationHoursArr && this.operationHoursArr.length > 0) {
-      this.formGroup.patchValue(this.operationHoursArr);
-    }
   }
 }
