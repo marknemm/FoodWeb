7fe4c877394e37b71f3a5147234c922ce0a1e5cb
Mark <marknemmer@gmail.com>
2019-09-28 19:02:33 -0400

    Fix warning messages for angular prod build. Fix server url (needs www).

--- client/src/app/account/forms/volunteer.form.ts
+++ client/src/app/account/forms/volunteer.form.ts
@@ -1,9 +1,7 @@
-import { Injectable } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { TypedFormGroup } from '../../data-structure/typed-form-group';
 import { Volunteer } from '../../../../../shared/src/interfaces/account/volunteer';
 
-@Injectable()
 export class VolunteerForm extends TypedFormGroup<Volunteer> {
 
   constructor(volunteer?: Partial<Volunteer>) {
