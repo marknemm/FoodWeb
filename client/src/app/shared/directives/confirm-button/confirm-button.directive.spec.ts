d70d674733152f96869202c2be85ce8806fb2e6c
Mark <marknemmer@gmail.com>
2019-11-17 19:42:02 -0500

    Remove original angular webapp.

--- client/src/app/shared/directives/confirm-button/confirm-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConfirmButtonDirective } from './confirm-button.directive';
-
-describe('ConfirmButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ConfirmButtonDirective(null);
-    expect(directive).toBeTruthy();
-  });
-});
--- client/src/app/shared/directives/display-edit-transition/display-edit-transition.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisplayEditTransitionDirective } from './display-edit-transition.directive';
-
-describe('DisplayEditTransitionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisplayEditTransitionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
