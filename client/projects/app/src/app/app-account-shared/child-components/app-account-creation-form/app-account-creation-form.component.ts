43eefa4069a52f819dcf86da9d65888ee003fc90
Mark Nemmer <marknemm@buffalo.edu>
2020-11-01 11:58:11 -0500

    Create AppTabsService and migrate AppAccountCreateFormComponent's tab management logic to the new service.

--- client/projects/app/src/app/app-account-shared/child-components/app-account-creation-form/app-account-creation-form.component.ts
+++ client/projects/app/src/app/app-account-shared/child-components/app-account-creation-form/app-account-creation-form.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RouterExtensions } from '@nativescript/angular';
-import { Focusable } from '~app/app-shared/interfaces/focusable';
 import { AppBackService } from '~app/app-shared/services/app-back/app-back.service';
+import { AppTabsService } from '~app/app-shared/services/app-tabs/app-tabs.service';
 import { AccountCreationFormBaseComponent } from '~web/account-shared/child-components/account-creation-form/account-creation-form.base.component';
 
 @Component({
   selector: 'foodweb-app-account-creation-form',
   templateUrl: './app-account-creation-form.component.html',
-  styleUrls: ['./app-account-creation-form.component.scss']
+  styleUrls: ['./app-account-creation-form.component.scss'],
+  providers: [AppTabsService]
 })
 export class AppAccountCreationFormComponent extends AccountCreationFormBaseComponent implements OnInit {
 
-  private _tabIdx = 0;
-  private _tabIdxHistory: number[] = [];
-
   constructor(
+    public tabsService: AppTabsService,
     protected _activatedRoute: ActivatedRoute,
     private _backService: AppBackService,
     private _routerExt: RouterExtensions,
@@ -28,30 +27,6 @@ export class AppAccountCreationFormComponent extends AccountCreationFormBaseComp
     this._backService.overrideBackUntil(this._destroy$).subscribe(
       () => this._androidBackOverride()
     );
-  }
-
-  get tabIdx(): number {
-    return this._tabIdx;
-  }
-
-  nextTab(): void {
-    this.toTab(this._tabIdx + 1);
-  }
-
-  nextTabAndFocus(focusable: Focusable): void {
-    this.nextTab();
-    focusable.focus();
-  }
-
-  prevTab(): void {
-    this.toTab(this._tabIdx - 1);
-  }
-
-  toTab(tabIdx: number): void {
-    if (this.tabIdx !== tabIdx && tabIdx >= 0) {
-      this._tabIdxHistory.push(this._tabIdx);
-      this._tabIdx = tabIdx;
-    }
   }
 
   backToAccountTypeSel(): void {
@@ -69,8 +44,8 @@ export class AppAccountCreationFormComponent extends AccountCreationFormBaseComp
    * NOTE: On iOS, the action bar back button should be collapsed/hidden on this page.
    */
   private _androidBackOverride(): void {
-    if (this._tabIdxHistory.length) {
-      this._tabIdx = this._tabIdxHistory.pop();
+    if (this.tabsService.tabIdxHistoryLength) {
+      this.tabsService.tabHistoryBack();
     } else if (this.accountType) {
       this.backToAccountTypeSel();
     } else {
