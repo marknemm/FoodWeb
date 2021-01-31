15262dbcbe480f784f86d99ab39eba6dd7f40fae
Mark <marknemmer@gmail.com>
2021-01-31 17:56:21 -0500

    Complete donation-hub-pledge list feature. Restrict visibility of donation hub pages to volunteer accounts.

--- client/projects/web/src/app/home/components/home/home.component.ts
+++ client/projects/web/src/app/home/components/home/home.component.ts
@@ -16,15 +16,19 @@ export class HomeComponent implements OnInit {
   private _featuredEvent: FeaturedEvent;
 
   constructor(
+    public sessionService: SessionService,
     private _activatedRoute: ActivatedRoute,
     private _authService: AuthenticationService,
     private _featuredEventsService: FeaturedEventsService,
     private _matDialog: MatDialog,
-    private _sessionService: SessionService
   ) {}
 
   get featuredEvent(): FeaturedEvent {
     return this._featuredEvent;
+  }
+
+  get donationHubFeatureVisible(): boolean {
+    return (!this.sessionService.isDonor && !this.sessionService.isReceiver);
   }
 
   ngOnInit() {
@@ -33,11 +37,15 @@ export class HomeComponent implements OnInit {
       if (!!this._activatedRoute.snapshot.queryParams['impersonationToken']) {
         this._authService.logout();
       }
-      LoginDialogComponent.openIfNotLoggedIn(this._sessionService, this._matDialog);
+      this.openLoginDialog();
     }
     this._featuredEventsService.getNextFeaturedEvent().subscribe(
       (featuredEvent: FeaturedEvent) => this._featuredEvent = featuredEvent
     );
+  }
+
+  openLoginDialog(): void {
+    LoginDialogComponent.openIfNotLoggedIn(this.sessionService, this._matDialog);
   }
 
 }
