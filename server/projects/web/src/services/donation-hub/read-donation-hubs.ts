0164a49615ebd70358e9792f9c1cec64c44b05c7
Mark <marknemmer@gmail.com>
2021-02-10 17:34:59 -0500

    Add job script for sending donation-hub reminders

--- server/projects/web/src/services/donation-hub/read-donation-hubs.ts
+++ server/projects/web/src/services/donation-hub/read-donation-hubs.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository, SelectQueryBuilder } from 'typeorm';
 import { AccountEntity } from '~entity';
-import { addOrder, addPagination, addWhere } from '~orm';
+import { addOrder, addPagination, addWhere, QueryMod } from '~orm';
 import { DateTimeHelper, DonationHubReadRequest, ListResponse } from '~shared';
 import { DonationHubEntity } from '~web/database/entity/donation-hub.entity';
 import { genListResponse, ListResponsePromise } from '~web/helpers/response/list-response';
@@ -39,19 +39,43 @@ export async function readMyDonationHubs(
  * @return A promise that resolves to a `DonationHubEntity` list response.
  */
 export async function readDonationHubs(request: DonationHubReadRequest): ListResponsePromise<DonationHubEntity> {
+  return queryDonationHubs(request).exec();
+}
+
+/**
+ * Queries a list of donation hubs from the database, and exposes a query modifier that may be used to
+ * modify the query before sending it to the database.
+ * @param request The request sent by the client specifying read filters & options.
+ * @return A query modifier which may be used to modify the query before reading donation hubs.
+ */
+export function queryDonationHubs(request: DonationHubReadRequest): QueryMod<DonationHubEntity> {
   const repository: Repository<DonationHubEntity> = getRepository(DonationHubEntity);
+  const queryBuilder: SelectQueryBuilder<DonationHubEntity> = _buildQuery(repository, request);
+  return new QueryMod<DonationHubEntity>(
+    queryBuilder,
+    async () => {
+      const [donationHubs, totalCount] = await queryBuilder.getManyAndCount();
+      return genListResponse(donationHubs, totalCount, request);
+    }
+  );
+}
+
+function _buildQuery(repository: Repository<DonationHubEntity>, request: DonationHubReadRequest): SelectQueryBuilder<DonationHubEntity> {
   const queryBuilder: SelectQueryBuilder<DonationHubEntity> = repository.createQueryBuilder('donationHub');
-  _addRelations(queryBuilder);
+  _addRelations(queryBuilder, request);
   _addFilters(queryBuilder, request);
   _addSorting(queryBuilder, request);
   addPagination(queryBuilder, request);
-  const [donationHubs, totalCount] = await queryBuilder.getManyAndCount();
-  return genListResponse(donationHubs, totalCount, request);
+  return queryBuilder;
 }
 
-function _addRelations(queryBuilder: SelectQueryBuilder<DonationHubEntity>): void {
+function _addRelations(queryBuilder: SelectQueryBuilder<DonationHubEntity>, request: DonationHubReadRequest): void {
   addDefaultAccountAssociations(queryBuilder, 'donationHub.volunteerAccount', 'hubAccount');
   queryBuilder.leftJoin('donationHub.contactOverride', 'contactOverride');
+  if (request.loadPledges) {
+    queryBuilder.leftJoinAndMapMany('donationHub.pledges', 'donationHub.pledges', 'pledge');
+    addDefaultAccountAssociations(queryBuilder, 'pledge.account', 'pledgeAccount');
+  }
 }
 
 function _addFilters(queryBuilder: SelectQueryBuilder<DonationHubEntity>, request: DonationHubReadRequest): void {
