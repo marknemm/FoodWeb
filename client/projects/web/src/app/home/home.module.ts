fe6760f4cfc8c72e32e2e5e1c21f48e6f92535f7
aghose <aghose@buffalo.edu>
2021-02-14 18:51:17 -0500

    Updated Readme per Mark's instructions and added a Login and signup button

--- client/projects/web/src/app/home/home.module.ts
+++ client/projects/web/src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { EventSharedModule } from '~web/event-shared/event-shared.module';
@@ -13,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     CommonModule,
     MatCardModule,
+    MatButtonModule,
     RouterModule.forChild([]),
     EventSharedModule,
     HeuristicsModule,
