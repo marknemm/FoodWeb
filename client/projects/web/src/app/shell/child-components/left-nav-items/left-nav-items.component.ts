56819f8c425ef5c1c85cb5de181cc2a95d123914
Mark <marknemmer@gmail.com>
2021-06-17 22:14:59 -0400

    Update navigation UI and add donation hub portal page.

--- client/projects/web/src/app/shell/child-components/left-nav-items/left-nav-items.component.ts
+++ client/projects/web/src/app/shell/child-components/left-nav-items/left-nav-items.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { faDonate, faGifts, faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons';
 import { DonationHelper } from '~shared';
-import { LeftNavService } from '~web/shell/services/left-nav/left-nav.service';
+import { LoginDialogComponent } from '~web/session/components/login-dialog/login-dialog.component';
 import { SessionService } from '~web/session/services/session/session.service';
 import { ConstantsService } from '~web/shared/services/constants/constants.service';
+import { LeftNavService } from '~web/shell/services/left-nav/left-nav.service';
 
 @Component({
   selector: 'foodweb-left-nav-items',
@@ -20,13 +22,19 @@ export class LeftNavItemsComponent implements OnInit {
     public constantsService: ConstantsService,
     public donationHelper: DonationHelper,
     public sessionService: SessionService,
-    private _leftNavService: LeftNavService
+    private _leftNavService: LeftNavService,
+    private _matDialog: MatDialog
   ) {}
 
   ngOnInit() {}
 
   _onNavigate(): void {
     this._leftNavService.toggle();
+  }
+
+  showLoginDialog(): void {
+    this._leftNavService.toggle();
+    LoginDialogComponent.openIfNotLoggedIn(this.sessionService, this._matDialog);
   }
 
 }
