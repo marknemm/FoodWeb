2f29e1d8ded315b7d2f94d66bf90462b90a0a073
Mark <marknemmer@gmail.com>
2021-07-01 21:50:32 -0400

    Update android project.

--- client/projects/hybrid/android/app/src/main/java/io/hybrid/foodweb/MainActivity.java
+++ client/projects/hybrid/android/app/src/main/java/io/hybrid/foodweb/MainActivity.java
@@ -1,15 +1,5 @@
 package io.hybrid.foodweb;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-}
+public class MainActivity extends BridgeActivity {}
