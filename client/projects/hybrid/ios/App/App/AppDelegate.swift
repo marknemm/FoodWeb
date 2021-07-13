7a4e0b42c7fbf156ff34ba62ea896fab3057141a
Mark Nemmer <marknemm@buffalo.edu>
2021-07-12 22:02:03 -0400

    Enable push notifications

--- client/projects/hybrid/ios/App/App/AppDelegate.swift
+++ client/projects/hybrid/ios/App/App/AppDelegate.swift
@@ -58,6 +58,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //            }
 //        })
 //    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+      NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
