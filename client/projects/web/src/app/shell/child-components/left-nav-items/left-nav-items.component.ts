eab518e5495d1f062fce76d86fae43e7f637aee1
Mark Nemmer <marknemmer@gmail.com>
2022-03-13 18:57:06 -0400

    Create FeatureFlagService for easily showing/hiding various features. Hide deliveries features form navigation by default (can be shown with showDeliveries=true feature flag).

--- client/projects/web/src/app/shell/child-components/left-nav-items/left-nav-items.component.ts
+++ client/projects/web/src/app/shell/child-components/left-nav-items/left-nav-items.component.ts
@@ -4,6 +4,7 @@ import { DonationHelper } from '~shared';
 import { AuthenticationService } from '~web/session/services/authentication/authentication.service';
 import { SessionService } from '~web/session/services/session/session.service';
 import { ConstantsService } from '~web/shared/services/constants/constants.service';
+import { FeatureFlagService } from '~web/shared/services/feature-flag/feature-flag.service';
 import { ShellService } from '~web/shell/services/shell/shell.service';
 
 @Component({
@@ -17,15 +18,24 @@ export class LeftNavItemsComponent implements OnInit {
   faGifts = faGifts;
   faHandHoldingHeart = faHandHoldingHeart;
 
+  private _showDeliveries = false;
+
   constructor(
     public constantsService: ConstantsService,
     public donationHelper: DonationHelper,
     public sessionService: SessionService,
     private _authService: AuthenticationService,
+    private _featureFlagService: FeatureFlagService,
     private _shellService: ShellService,
   ) {}
 
-  ngOnInit() {}
+  get showDeliveries(): boolean {
+    return this._showDeliveries;
+  }
+
+  ngOnInit(): void {
+    this._showDeliveries = this._featureFlagService.hasTruthyFeatureFlag('showDeliveries');
+  }
 
   _onNavigate(): void {
     this._shellService.toggleLeftNav();
